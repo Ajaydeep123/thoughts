bf520d84acefdd6cd0d06de1d49afbafb905599e
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-07-15 18:14:28 +0530

    📝 Db Setup and Schema defined

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log('Already Connected to the db')
+        return;
+    }
+
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+        connection.isConnected = db.connections[0].readyState;
+        console.log('Database connected');
+    }catch(error){
+        console.error('Error connecting to db', error);
+        process.exit(1)        
+    }
+}
+
+export default dbConnect;
