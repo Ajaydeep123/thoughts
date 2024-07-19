25ec97b955fd8cab95e8467bd9be327bb118bc24
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-07-19 23:20:48 +0530

    📝 message api setup

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,10 @@
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { Message } from "@/model/User";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    const {username, content} = await request.json();
+    
+}
