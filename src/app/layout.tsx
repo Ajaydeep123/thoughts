07026dbd8d113e1e93af13316e02d2c0a972a120
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-07-28 16:50:44 +0530

    ✅ 📝 Auth Pages Implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider><body className={inter.className}>{children}</body></AuthProvider>
+      <AuthProvider>
+      <body className={inter.className}>{children}
+        <Toaster/>
+      </body>
+      </AuthProvider>
     </html>
   );
 }
