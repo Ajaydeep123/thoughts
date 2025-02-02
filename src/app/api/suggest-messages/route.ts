7a72d7412c9daa19ac8f68aea3e612daf8bb2578
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2025-02-02 13:43:41 +0530

    Updated groq model

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     const prompt = "Create a list of three open-ended and engaging questions keep them at most of 20 words strictly formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment and strictly just give the responsea no formality questions, just the answers strictly in a single string, also don't include 'Here are three open-ended and engaging questions for an anonymous social messaging platform:' strictly. Also, ensure that you are not repeating the same questions in the output strictly.";
 
   const result = await streamText({
-    model: groq('llama-3.1-70b-versatile'),
+    model: groq('llama-3.3-70b-versatile'),
     prompt,
   });
 
